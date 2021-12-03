307bd887146535b4e36aa3e2f5c9a5e608c9af2f
Mithlesh Kumar <mithlesh.9m@gmail.com>
2021-12-03 16:24:41 +0530

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import checkListener from './scripts/check.js'
+import checkListener from './scripts/check.js'; 
 
 const todoList = localStorage.getItem('todoList')
-? JSON.parse(localStorage.getItem('todoList'))
-: [];
+  ? JSON.parse(localStorage.getItem('todoList'))
+  : [];
 const list = document.getElementById('todo-list');
 const btn = document.querySelector('#arrow');
 const input = document.querySelector('.input');
@@ -13,27 +13,24 @@ btn.addEventListener('click', () => {
   });
   input.value = '';
   localStorage.setItem('todoList', JSON.stringify(todoList));
-  render ();
+  function render() {
+    list.innerHTML = '';
+    todoList.forEach((data, i) => {
+      list.innerHTML += `
+      <hr>
+      <div class="list-info" id=${i}>
+      <div class='${data.done ? 'checked' : ""}'>
+      <input type="checkbox" class="checkbox" ${data.done ? 'checked' : ''}>
+      <li class='text'>${data.value}</li>
+      </div>
+      <span>
+      <i class="fas fa-ellipsis-v"></i>
+      <i class="far fa-trash-alt" id='rm'></i>
+      </span>
+      </div>
+      `;
+    checkListener(todoList);
+    });
+  }
+   render ();
 });
-
-function render () {
-  list.innerHTML = '';
-  todoList.forEach((data, i) => {
-    list.innerHTML += `
-    <hr>
-    <div class="list-info" id=${i}>
-    <div class='${data.done ? 'checked' : ""}'>
-    <input type="checkbox" class="checkbox" ${data.done ? 'checked' : ''}>
-    <li class='text'>${data.value}</li>
-    </div>
-    <span>
-    <i class="fas fa-ellipsis-v"></i>
-    <i class="far fa-trash-alt" id='rm'></i>
-    </span>
-    </div>
-    `;
-  checkListener(todoList);
-  });
-}
- render ();
-
--- src/scripts/check.js
+++ src/scripts/check.js
@@ -1,9 +1,8 @@
-export default function checkListener (todoList) {
-    const elems = document.querySelectorAll('.checkbox');
-    elems.forEach(v => v.addEventListener('change', () => {
+export default function checkListener(todoList) {
+  const elems = document.querySelectorAll('.checkbox');
+  elems.forEach(v => v.addEventListener('change', (() => {
     v.parentNode.classList.toggle('checked');
     todoList[v.parentElement.parentElement.id].done = v.checked;
     localStorage.setItem('todoList', JSON.stringify(todoList));
-    }));
-}
-
+  }));
+};
