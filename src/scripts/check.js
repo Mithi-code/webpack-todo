181ddd555e4da52b5ac5d7e77fdf8ca36ca4d8e8
Mithlesh Kumar <mithlesh.9m@gmail.com>
2021-12-03 18:30:32 +0530

    fix linter

--- src/scripts/check.js
+++ src/scripts/check.js
@@ -1,10 +1,10 @@
 export default function checkListener(todoList) {
   const elems = document.querySelectorAll('.checkbox');
-  elems.forEach(v => {
+  elems.forEach((v) => {
     v.addEventListener('change', () => {
-        v.parentNode.classList.toggle('checked');
-        todoList[v.parentElement.parentElement.id].done = v.checked;
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-      })
+      v.parentNode.classList.toggle('checked');
+      todoList[v.parentElement.parentElement.id].done = v.checked;
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+    });
   });
 }
